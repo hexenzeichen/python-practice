9ad080e502985e775ff31f7988ab03175e001f3b
hexenzeichen <ndrkrpv@gmail.com>
2025-09-14 18:45:43 +0300

    Add the inspect example

---
+++ inspect.py
@@ -0,0 +1,12 @@
+def inspect(x):
+    if x == 0:
+        print("is zero")
+    elif x > 0:
+        print("is positive")
+    elif x < 0:
+        print("is negative")
+    else:
+        print("is unlike anything I've ever seen")
+
+inspect(0)
+inspect(-115)
