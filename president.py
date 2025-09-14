063e7e188c1cc93a5e3845632d9ea01832f367dd
hexenzeichen <ndrkrpv@gmail.com>
2025-09-14 18:36:05 +0300

    Make can_run_for_president more specific

--- president.py
+++ president.py
@@ -1,5 +1,7 @@
-def can_run_for_president(age):
-    return age >=35
+def can_run_for_president(age, is_natural_born_citizen):
+    """Can someone of the given age and citizenship status run for president in the US?"""
+    # The US Constitution says you must be a natural born citizen *and* at least 35 years old
+    return is_natural_born_citizen and age >=35
 
 
-print("Can a 19-year-old run for president?", can_run_for_president(19))
+print("Can this dude run for president?", can_run_for_president(True, 19))
