265ece308e7c7fd62ed57fd2cf4bc12d154f6cc5
hexenzeichen <ndrkrpv@gmail.com>
2025-10-08 09:10:17 +0300

    Add to_bytes converter

---
+++ to_bytes.py
@@ -0,0 +1,11 @@
+def to_bytes(bytes_or_str):
+    if isinstance(bytes_or_str, str):
+        value = bytes_or_str.encode('utf-8')
+    else:
+        value = bytes_or_str
+    return value
+
+print(repr(to_bytes(b'foo')))
+print(repr(to_bytes('bar')))
+
+
