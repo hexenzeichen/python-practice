e6274150c02e007a3d6543150401e427635bc7ec
hexenzeichen <ndrkrpv@gmail.com>
2025-10-09 09:43:05 +0300

    Add the exercise 3

---
+++ exercises/ex3.py
@@ -0,0 +1,43 @@
+# writes the line
+print("I will now count my chickens:")
+
+# writes the line. I can probably provide several lines to print
+print("Hens", 25 + 30 / 6)
+# does the same
+print("Roosters", 100 - 25 * 3 % 4)
+
+# writes the line again
+print("Now I will count the eggs:")
+
+# calculates and writes the line
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# writes the line as always
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# writes the boolean result of the operation
+print(3 + 2 < 5 - 7)
+
+# writes the line and the result of the calculation
+print("What is 3 + 2?", 3 + 2)
+
+# writes the line and the result of the calculation
+print("What is 5 - 7?", 5 - 7)
+
+# the same
+print("Oh, that's why it's False.")
+
+# the same
+print("How about some more.")
+
+# the same
+print("Is it greater?", 5 >= -2)
+
+# the same
+print("Is it greater or equal?", 5 >= -2)
+
+# the same
+print("Is it less or equal?", 5 <= -2)
+
+
+
